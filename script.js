37bc494346dac59328bf89983c33dc20e96e4ae1
Kaofang <benyapa_kh@cmu.ac.th>
2021-07-22 14:09:21 +0700

    addtask

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-/* Your code here */
+let currentInput = ''
+
+//input task
+const input = document.createElement('input')
+input.addEventListener( 'input', ev => {
+	currentInput = ev.target.value
+})
+
+//list (ul, li)
+const ul = document.createElement('ul')
+
+//add task button
+const btn = document.createElement('button')
+btn.innerHTML="Add Task"
+btn.addEventListener('click', () => {
+	const li = document.createElement('li')
+	li.innerHTML = currentInput
+	ul.append(li)
+})
+//show everything
+document.body.append(input)
+document.body.append(btn)
+document.body.append(ul)
